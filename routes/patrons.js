062fd3ab617fdbadbfc24f3487cdcac4ccc5376d
anoopmundathan <anoop.mundathan@gmail.com>
2017-01-31 08:32:45 +0000

    List all patrons

--- routes/patrons.js
+++ routes/patrons.js
@@ -1,19 +1,26 @@
 var express = require('express');
+
+var Books = require('../models').Books;
+var Loans = require('../models').Loans;
 var Patrons = require('../models').Patrons;
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
-	Patrons.findAll().then(function(patrons) {
+// GET all patrons
+router.get('/', function(req, res, next) {
+	Patrons.findAll({
+		order: [['createdAt', 'DESC']],
+	}).then(function(patrons) {
 		res.render('all_patrons', {
 			patrons: patrons
 		});
 	});
 });
 
-router.post('/', function(req, res) {
+// POST - create new patron
+router.post('/', function(req, res, next) {
 	Patrons.create(req.body).then(function(patron) {
-		res.render('patron_detail', {patron : patron});
+		res.redirect('/patrons');
 	}).catch(function(err) {
 		if(err.name === "SequelizeValidationError") {
 			res.render('new_patron', {
@@ -28,8 +35,43 @@ router.post('/', function(req, res) {
 	})
 });
 
-router.get('/new', function(req, res) {
+// GET new book route
+router.get('/new', function(req, res, next) {
 	res.render('new_patron', {patron : Patrons.build()});
+});
+
+// GET individual patron
+router.get('/:id', function(req, res, next) {
+	Patrons.findById(req.params.id).then(function(patron) {
+		/*
+		 * Set Associations
+		 */
+		Loans.belongsTo(Books, {foreignKey: 'book_id'});
+		Loans.belongsTo(Patrons, {foreignKey: 'patron_id'});
+		/*
+		 * SELECT * 
+		 * FROM LOANS A 
+		 * INNER JOIN BOOKS B 
+		 * 		ON A.BOOK_ID = B.ID
+		 * INNER JOIN PATRONS C
+		 *      ON A.PATRON_ID = C.ID 
+		 * WHERE 
+		 *      A.BOOK_ID = REQ.PARAMS.ID;
+		*/
+
+		Loans.findAll({
+			include: [
+					  {model: Books,required: true}, 
+					  {model: Patrons,required: true}
+					 ],
+			where: {
+				patron_id: req.params.id
+			}
+		}).then(function(data) {
+			res.render('patron_detail', {patron: patron, loans: data});
+		});
+		
+	});
 });
 
 module.exports = router;
